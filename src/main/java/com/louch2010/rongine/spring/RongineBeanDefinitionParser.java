a09d786a0733064ec17a498585f58529e1ce82f5
Louch2010 <1175889450@qq.com>
2016-11-17 18:16:58 +0800

    集成spring

---
+++ src/main/java/com/louch2010/rongine/spring/RongineBeanDefinitionParser.java
@@ -0,0 +1,14 @@
+package com.louch2010.rongine.spring;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+public class RongineBeanDefinitionParser implements BeanDefinitionParser{
+
+	public BeanDefinition parse(Element element, ParserContext parserContext) {
+		
+		return null;
+	}
+}
---
+++ src/main/java/com/louch2010/rongine/spring/RongineNamespaceHandler.java
@@ -0,0 +1,10 @@
+package com.louch2010.rongine.spring;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class RongineNamespaceHandler extends NamespaceHandlerSupport{
+
+	public void init() {
+		
+	}
+}
