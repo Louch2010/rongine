e3f7ae10c236cf7e39e7e0bc070fde7e38f6eeb8
Louch2010 <1175889450@qq.com>
2016-11-22 11:01:24 +0800

    集成spring

--- src/main/java/com/louch2010/rongine/spring/RongineBeanDefinitionParser.java
+++ src/main/java/com/louch2010/rongine/spring/RongineBeanDefinitionParser.java
@@ -1,14 +1,28 @@
 package com.louch2010.rongine.spring;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 public class RongineBeanDefinitionParser implements BeanDefinitionParser{
+	
+	private static final Log logger = LogFactory.getLog(RongineBeanDefinitionParser.class);
+    private final Class<?> beanClass;
+    private final boolean required;
+	
+	public RongineBeanDefinitionParser (Class<?> beanClass, boolean required) {
+		this.beanClass = beanClass;
+		this.required = required;
+	}
 
-	public BeanDefinition parse(Element element, ParserContext parserContext) {
-		
+	public BeanDefinition parse(Element element, ParserContext context) {
+		RootBeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setBeanClass(beanClass);
+        beanDefinition.setLazyInit(false);
 		return null;
 	}
 }
--- src/main/java/com/louch2010/rongine/spring/RongineNamespaceHandler.java
+++ src/main/java/com/louch2010/rongine/spring/RongineNamespaceHandler.java
@@ -2,9 +2,20 @@ package com.louch2010.rongine.spring;
 
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
+import com.louch2010.rongine.config.ApplicationConfig;
+import com.louch2010.rongine.config.DistributedRegisterConfig;
+import com.louch2010.rongine.config.ProtocolConfig;
+import com.louch2010.rongine.config.ReferenceConfig;
+
 public class RongineNamespaceHandler extends NamespaceHandlerSupport{
 
 	public void init() {
-		
+		 registerBeanDefinitionParser("application", new RongineBeanDefinitionParser(ApplicationConfig.class, true));
+		 registerBeanDefinitionParser("registry", new RongineBeanDefinitionParser(DistributedRegisterConfig.class, true));
+		 registerBeanDefinitionParser("protocol", new RongineBeanDefinitionParser(ProtocolConfig.class, true));
+		 /*registerBeanDefinitionParser("provider", new RongineBeanDefinitionParser(ProviderConfig.class, true));
+	     registerBeanDefinitionParser("consumer", new RongineBeanDefinitionParser(ConsumerConfig.class, true));
+	     registerBeanDefinitionParser("service", new RongineBeanDefinitionParser(ServiceBean.class, true));*/
+	     registerBeanDefinitionParser("reference", new RongineBeanDefinitionParser(ReferenceConfig.class, false));
 	}
 }
